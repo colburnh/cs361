0e41b4767b7116af186387ebd859e507f1274a0e
jabate13 <39861552+jabate13@users.noreply.github.com>
2020-08-12 21:10:17 -0700

    Added Schedules to views

--- HogwartsDatabase/dbo/Stored Procedures/usp_get_scheduledclasses.sql
+++ HogwartsDatabase/dbo/Stored Procedures/usp_get_scheduledclasses.sql
@@ -6,8 +6,23 @@ BEGIN
 	SET NOCOUNT ON;
 
 	SELECT 
-		*
-	FROM
-		ScheduledClasses
+		sc.ScheduledClassID,
+		p.LastName AS 'Professor',
+		c.Name AS 'Class',
+		h.HouseName AS 'House',
+		StartTime,
+		EndTime,
+		DaysoftheWeek,
+		Year
+	FROM 
+		ScheduledClasses sc
+		JOIN ProfessorClasses pc
+			ON sc.ProfessorClassID = pc.ProfessorClassID
+		JOIN Classes c
+			ON pc.ClassID = c.ClassId
+		JOIN Professors p
+			ON pc.ProfessorID = p.ProfessorID
+		JOIN Houses h
+			ON sc.HouseID = h.HouseID
 
 END
--- HogwartsWebApp/hogwartsweb/sqlserverconnect/models.py
+++ HogwartsWebApp/hogwartsweb/sqlserverconnect/models.py
@@ -35,14 +35,10 @@ class classessqlserverconn(models.Model):
 
 class schedulesqlserverconn(models.Model):
     ScheduledClassID = models.IntegerField()
-    ProfessorClassID = models.IntegerField()
-    HouseID = models.IntegerField()
+    Professor = models.CharField(max_length=100)
+    Class = models.CharField(max_length=100)
+    House = models.CharField(max_length=100)
     StartTime = models.DateTimeField()
     EndTime = models.DateTimeField()
     DaysoftheWeek = models.CharField(max_length=100)
     Year = models.IntegerField()
-    MaxOccupancy = models.IntegerField()
-    Occupancy = models.IntegerField()
-    RemainingSpots = models.IntegerField()
-    Location = models.CharField(max_length=100)
-    NumberofHouses = models.IntegerField()
